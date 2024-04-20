91f1132676578123032ee84693b107c8ed8aaac6
Sailor1314 <zhongfeng.wu@edu.sait.ca>
2024-04-20 15:26:24 -0600

    connect database

--- node-api-postgres/index.js
+++ node-api-postgres/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
 const app = express()
+const { Pool } = require('pg')
 const port = 3000
+
+const pool = new Pool({//client
+  user: 'me',
+  host: 'localhost', //remote server
+  database: 'api',
+  password: 'password',
+  port: 5432,//remote server port
+},
+);
 
 app.use(bodyParser.json())
 app.use(
@@ -11,9 +22,45 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
+
+  getUsers(request, response)
+  //getUserById(request, response)
+    //response.json({ info: 'Node.js, Express, and Postgres API' })
   })
 
-app.listen(port, () => {
+app.listen(3000, () => {
 console.log(`App running on port ${port}.`)
 })
+
+app.get('/test', (request, response) => {
+  
+    response.json({ info: 'here is test'})
+  })
+
+
+console.log(555)
+
+
+
+console.log(1111)
+
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC',
+   (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+   })
+}
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
